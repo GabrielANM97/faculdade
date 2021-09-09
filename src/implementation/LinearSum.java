1321284b82395c29fc920a04b26962e27e2b3a5a
GabrielANM <bilbor8@gmail.com>
2021-09-08 22:34:58 -0300

    enviando atividade para o git

---
+++ src/implementation/LinearSum.java
@@ -0,0 +1,11 @@
+package implementation;
+
+
+public class LinearSum {
+	public static  int linearSum(int[] A, int n) {
+		if (n == 1) 
+			return A[0];
+		
+		else return linearSum(A, n - 1) + A[n - 1];		
+	}
+}
